15d3f16678f5c64e5bef5944baf27f70b5c81082
jgong4694 <110438363+jgong4694@users.noreply.github.com>
2023-03-29 12:24:37 +0900

    Delete tetrisGo directory
    삭제

--- tetrisGo/app/src/main/java/com/example/tetrisgo/Block.kt
+++
@@ -1,8 +0,0 @@
-package com.example.tetrisgo
-
-data class Block(
-    var x: Int,
-    var y: Int,
-    var shape: Array<BooleanArray>,
-    var color: Int
-)
--- tetrisGo/app/src/main/java/com/example/tetrisgo/gameObject.kt
+++
@@ -1,8 +0,0 @@
-package com.example.tetrisgo
-
-object gameObject {
-    private const val GAME_ROWS = 20
-    private const val GAME_COLS = 10
-    private const val BLOCK_SIZE = 4
-    private const val BLOCK_FALL_DELAY = 500L
-}
